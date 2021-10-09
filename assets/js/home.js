3584d86a5a236e3f04e63c8800b3220af258d9de
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-10-10 03:56:18 +0530

    Simple Login/Register for fire clients

---
+++ assets/js/home.js
@@ -0,0 +1,73 @@
+let user
+
+let schemaBuilder = lf.schema.create("userData", 1)
+let transactionSchemaBuilder = lf.schema.create("transactionData", 2)
+
+schemaBuilder
+    .createTable("User")
+    .addColumn("id", lf.Type.INTEGER)
+    .addColumn("_id", lf.Type.STRING)
+    .addColumn("email", lf.Type.STRING)
+    .addColumn("firstName", lf.Type.STRING)
+    .addColumn("lastName", lf.Type.STRING)
+    .addColumn("profilePic", lf.Type.STRING)
+    .addPrimaryKey(["id"], true);
+
+transactionSchemaBuilder
+    .createTable("Transactions")
+    .addColumn("id", lf.Type.INTEGER)
+    .addColumn("token", lf.Type.STRING)
+    .addColumn("url", lf.Type.STRING)
+    .addColumn("time", lf.Type.STRING)
+    .addColumn("method", lf.Type.STRING)
+    .addPrimaryKey(["id"], true);
+
+let userDb
+let item
+
+let transactionDb
+let transaction
+
+schemaBuilder.connect().then(async (db) => {
+    userDb = db;
+    item = db.getSchema().table("User")
+    user = await getUserDetails()
+    if(user != undefined) {
+        console.log(user)
+    }
+    else {
+        // userDb.delete().from(item).exec() // Clear session or Logout Query
+        window.location.href = "/login.html"
+    }
+})
+
+async function createItem(data) {
+	let row = item.createRow(data)
+	return await userDb.insertOrReplace().into(item).values([row]).exec().catch(err => console.log(err))
+}
+
+function getUserDetails() {
+    return new Promise((resolve, reject) => {
+        let user;
+        userDb
+		.select()
+		.from(item)
+		.exec()
+        .then(res => {
+            user = res.at(-1);
+            resolve(user)
+        })
+    })
+}
+
+async function getTransactions() {
+    return new Promise((resolve, reject) => {
+        transactionDb
+		.select()
+		.from(transaction)
+		.exec()
+        .then(res => {
+            resolve (res)
+        })
+    })
+}
---
+++ assets/js/pure.js
@@ -0,0 +1,78 @@
+function returnUserCard(user) {
+	return `<div class="userContainer">
+                <div class="transparentCard">
+                    <img src="${user.profilePic}" alt="Profile Pic">
+                </div>
+                <div class="userCard">
+                    <div class="nameContainer">
+                        <span class="firstName">${user.firstName}</span>
+                        <span class="lastName">${user.lastName}</span>
+                    </div>
+                    <div class="email"><a href="mailto:${user.email}" aria-label="Email">${user.email}</a></div>
+                </div>
+            </div>`
+}
+
+function getDomain(url) {
+    let newUrl = new URL(url)
+    return newUrl.hostname
+}
+
+function timeDifference(current, previous) {
+	var msPerMinute = 60 * 1000;
+	var msPerHour = msPerMinute * 60;
+	var msPerDay = msPerHour * 24;
+	var msPerMonth = msPerDay * 30;
+	var msPerYear = msPerDay * 365;
+
+	var elapsed = current - previous;
+
+	if (elapsed < msPerMinute) {
+		if (elapsed / 1000 < 30) return "Just now";
+		return Math.round(elapsed / 1000) + "s";
+	} else if (elapsed < msPerHour) {
+		return Math.round(elapsed / msPerMinute) + "m";
+	} else if (elapsed < msPerDay) {
+		return Math.round(elapsed / msPerHour) + "h";
+	} else if (elapsed < msPerMonth) {
+		return Math.round(elapsed / msPerDay) + "d";
+	} else if (elapsed < msPerYear) {
+		return Math.round(elapsed / msPerMonth) + "month";
+	} else {
+		return Math.round(elapsed / msPerYear) + "y";
+	}
+}
+
+function showListOfTransactions (data) {
+    let html = ""
+    // reverse the data
+
+    data.reverse().forEach(element => {
+
+        let domain = getDomain(element.url)
+        let timeAgo = timeDifference(new Date(), new Date(element.time))
+        let token = element.token
+
+        html += `<div class="signup" data-id="${token}">
+                    <div class="signupHeader" data-id="${token}">
+                        <div class="signupDomain" data-id="${token}"> <span>${domain}</span> </div>
+                        <div class="signupTime" data-id="${token}"> ${timeAgo} </div>
+                    </div>
+                    <div class="signupInfo" data-id="${token}">
+                        <div class="signUpInfoContainer" data-id="${token}">
+                            <div class="fullSignupDomain" data-id="${token}">
+                                <span>${element.url}</span>
+                            </div>
+                            <div class="fullTimeContainer" data-id="${token}">
+                                <span>${new Date(element.time)}</span>
+                            </div>
+                            <div class="signupMethod" data-id="${token}">
+                                <span>Method: ${element.method}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+    });
+
+    return html
+}
